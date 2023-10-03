6ae4318f25e1e05785b2061b6a8592e73aa81b32
DimaMarkow <dimamarkow@gmail.com>
2023-10-03 14:33:34 +0300

    Plus reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { getReviewsById } from 'services/moviesAPI';
 
 const Reviews = () => {
-  return <div>Reviews</div>;
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    getReviewsById(movieId)
+      .then(data => {
+        setReviews([...data.results]);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  }, [movieId]);
+
+  return (
+    <>
+      {isLoading && <div>...Loading</div>}
+      <ul class="list-group">
+        {reviews.map(review => {
+          return (
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">{review.author}</div>
+                {review.content}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default Reviews;
